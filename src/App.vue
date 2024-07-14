9e292e1b21920d811b8f61eae5af22fc1aafce5e
Abduqodir0205 <abduqodirdeveloper@gmail.com>
2024-07-15 01:08:26 +0500

    ss

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,20 @@ import Sidebar from "./components/sidebar.vue";
 export default {
   data() {
     return {
-      cards: ["moon.png", "moon.png", "moon.png", "moon.png"]
+      /* 
+        AbdiqodirDev.
+
+        7/14/2024 14:04   
+
+        for tsiklda chiqarmoqchi bo'lgan rasmlar chiqmadi. Mani fikrimcha tsikl to'g'ri ishlayapti va rasmni src ga to'g'ri manzil boryapti.
+      
+      */
+      // cards: [
+      //   {url: "/assets/images/png/card1.png"},
+      //   {url: "/assets/images/png/card2.png"},
+      //   {url: "/assets/images/png/card3.png"},
+      //   {url: "/assets/images/png/card4.png"}
+      // ]
     };
   },
   name: "HeroSection",
@@ -26,14 +39,16 @@ export default {
     t(key) {
       return translate(key);
     },
-    getCardImage(card) {
-      try {
-        return require(`./assets/images/png/${card}`);
-      } catch (error) {
-        console.error(`Image not found: ./assets/images/png/${card}.png`);
-        return '';
-      }
-    },
+    // getCardImage(card) {
+    //   try {
+    //     console.log(card)
+    //     return card;
+    //   } catch (error) {
+    //     console.log(card)
+    //     console.error(card);
+    //     return '';
+    //   }
+    // },
   },
 };
 </script>
@@ -86,22 +101,55 @@ export default {
     </div>
 
     <div class="container">
-      <section class="section2" >
+      <section class="section2">
         <div class="section2__content">
           <h1>{{ t("about_us") }}</h1>
           <p>{{ t("about_us-label") }}</p>
 
-          <div class="section2__content-imgCollection" style="background-color: red">
-            <!-- <div v-for="(item, index) in cards"  class="section2__content-imgCollection-card"> -->
-              <!-- <img   src="./assets/images/png/card1.png" alt=""> -->
-              <!-- <img   :src="`./assets/images/png/${item}.png`" alt=""> -->
-              
-            <!-- </div> -->
-            <div v-for="(item, index) in cards" :key="index" class="section2__content-imgCollection-card" style="background-color: aliceblue">
-              <img :src="getCardImage(item)" alt="">
+          <div class="section2__content-imgCollection">
+            <div class="section2__content-imgCollection-card">
+              <img src="./assets/images/png/card1.png" alt="" />
+            </div>
+            <div class="section2__content-imgCollection-card">
+              <img src="./assets/images/png/card2.png" alt="" />
+            </div>
+            <div class="section2__content-imgCollection-card">
+              <img src="./assets/images/png/card3.png" alt="" />
+            </div>
+            <div class="section2__content-imgCollection-card">
+              <img src="./assets/images/png/card4.png" alt="" />
+            </div>
+
+            <!-- tsikl -->
+            <!-- <div v-for="(item, index) in cards" :key="index" class="section2__content-imgCollection-card">
+              <img :src="getCardImage(item.url)" alt="">
+            </div> -->
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <div class="container">
+      <section class="section3">
+        <h1>{{ t("section3_portfolio") }}</h1>
+        <div class="section3__portfolio-content">
+          <div class="card" v-for="n in 7" :key="n">
+            <!-- <img src="./assets/images/png/card1.png" alt="Card Image" /> -->
+            <div class="card-info">
+              <p>Card {{ n }} Information</p>
+            </div>
+            <div class="more">
+              {{ t("section3_more-txt") }}
+              <span>
+                <img src="./assets/images/png/right.png" alt="" />
+              </span>
             </div>
           </div>
         </div>
+        <button class="section3__btn">
+          <span><img src="./assets/images/png/right.png" alt=""></span>
+          {{ t("section3_btn-txt") }}
+        </button>
       </section>
     </div>
   </main>
@@ -285,13 +333,102 @@ main {
         -ms-overflow-style: none;
         scrollbar-width: none;
         &-card {
-          height: 50rem;
+          height: 30rem;
           position: relative;
           width: 25%;
+          // width: 300px;
           transition: all 0.3s ease;
           flex-shrink: 0;
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
+        }
+        &-card:hover {
+          width: 50%;
+          transition: all 0.3s ease;
         }
       }
+    }
+  }
+
+  .section3 {
+    margin-top: 16rem;
+
+    &__portfolio-content {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-auto-rows: 16.5vw;
+      grid-gap: 2rem;
+      gap: 2rem;
+    }
+    .card:first-child {
+      grid-row: 1 / 3;
+    }
+    .card:last-child {
+      grid-column: 2 / -1;
+    }
+    .card {
+      font-size: 1.4rem;
+      position: relative;
+      // width: 200px;
+      // height: 400px;
+      border-radius: 1rem;
+      overflow: hidden;
+      transition: transform 0.3s ease;
+      background: url("./assets/images/png/card1.png");
+      cursor: pointer;
+      transition: background 0.3s ease; /* Add transition for smooth gradient change */
+      &:hover {
+        background: linear-gradient(
+            160deg,
+            #d1329193,
+            rgba(255, 192, 203, 0) 36% 66%,
+            #d132917c
+          ),
+          url("./assets/images/png/card1.png");
+      }
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+      .card-info {
+        position: absolute;
+        // top: 1rem;
+        left: 1rem;
+        color: white;
+        // padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+      }
+      .more {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        position: absolute;
+        bottom: 1rem;
+        right: -50%;
+        color: white;
+        padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+        transition: right 0.3s ease;
+      }
+
+      &:hover {
+        .more {
+          right: 1rem;
+        }
+      }
+    }
+    &__btn {
+      cursor: pointer;
+      text-transform: capitalize;
+      font-size: 1.6rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #af1471,;
     }
   }
 }
